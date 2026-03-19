3d96a5129ebd0bf98729ea55d286081daa39535c
admin <chris@postfmly.com>
2026-03-19 07:11:32 -0500

    style: remove color

--- packages.sh
+++ packages.sh
@@ -6,29 +6,28 @@ _backend_url="https://git.postfmly.com/api/v1/packages/$GITHUB_VAULT_USER/contai
 _token=$(curl --request GET \
   --location \
   --silent \
+  --fail \
   --url https://vault.postfmly.com/v1/kv/data/github \
   --header "X-Vault-Token: $GITHUB_VAULT_TOKEN" \
   | jq -r .data.data.key)
 
-_green="\e[1;92m"
-_yellow="\e[93m"
-_nc="\e[0m"
-
-echo -e "✨ ${_green}m00d Packages${_nc} ✨\n"
+echo -e "✨ m00d Packages ✨\n"
 
 for _pkg in latest amd64 arm64; do
-  echo -e "   ╰› ${_yellow}Deleting ${_pkg^^} frontend package${_nc}"
+  echo -e "   ╰› Deleting ${_pkg^^} frontend package"
   curl --request DELETE \
     --location \
     --silent \
+    --fail \
     --url "${_frontend_url}${_pkg}" \
     --header "authorization: token $_token" \
     --output /dev/null
 
-  echo -e "   ╰› ${_yellow}Deleting ${_pkg^^} backend package${_nc}"
+  echo -e "   ╰› Deleting ${_pkg^^} backend package"
   curl --request DELETE \
     --location \
     --silent \
+    --fail \
     --url "${_backend_url}${_pkg}" \
     --header "authorization: token $_token" \
     --output /dev/null
@@ -36,11 +35,8 @@ for _pkg in latest amd64 arm64; do
   echo
 done
 
-echo -e "✔️  ${_green}Done${_nc}"
+echo -e "✔️  Done"
 
 unset _frontend_url
 unset _backend_url
 unset _token
-unset _green
-unset _yellow
-unset _nc
